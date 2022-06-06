51a1c849b9b236451232ebf124829a3b9681eaab
Tori Meyer <mey20010@byui.edu>
2022-06-06 09:32:37 -0600

    Copied from Articulate Unit04

---
+++ unit04-greed/Game/Casting/Artifact.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Unit04.Game.Casting
+{
+    /// <summary>
+    /// <para>An item of cultural or historical interest.</para>
+    /// <para>
+    /// The responsibility of an Artifact is to provide a message about itself.
+    /// </para>
+    /// </summary>
+    public class Artifact : Actor
+    {
+        private string message = "";
+
+        /// <summary>
+        /// Constructs a new instance of Artifact.
+        /// </summary>
+        public Artifact()
+        {
+        }
+
+
+        /// <summary>
+        /// Gets the artifact's message.
+        /// </summary>
+        /// <returns>The message as a string.</returns>
+        public string GetMessage(){
+            return message;
+        }
+
+
+        /// <summary>
+        /// Sets the artifact's message to the given value.
+        /// </summary>
+        /// <param name="message">The given message.</param>
+        public void SetMessage(string message){
+            this.message = message;
+        }
+    }
+}
